f1e103fee02a317b6a51245a426238fc4664bf3a
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-06-10 15:19:30 -0700

    Time: 62 ms (66.16%), Space: 63.1 MB (19.46%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -4,7 +4,8 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         for(int i = 0; i<temperatures.length; i++){
             while(!stack.isEmpty() && temperatures[stack.peek()]<temperatures[i]){
-                ans[stack.peek()] = i-stack.pop();
+                int last = stack.pop();
+                ans[last] = i-last;
             }
             stack.push(i);
         }
