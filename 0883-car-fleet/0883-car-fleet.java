686deaa4709ee7127e6d76abe3ee50f27a2b2559
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-06-10 20:53:20 -0700

    Time: 11 ms (99.97%), Space: 58.2 MB (37.07%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -1,22 +1,17 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int[][] pair = new int[position.length][2];
-        for (int i = 0; i < position.length; i++) {
-            pair[i][0] = position[i];
-            pair[i][1] = speed[i];
+        float[] time = new float[target+1];
+        for (int i = 0; i < speed.length; i++) {
+            time[position[i]] = (float)(target-position[i])/speed[i];
         }
-        Arrays.sort(pair, (a, b) -> Integer.compare(b[0], a[0]));
-        Stack<Double> stack = new Stack<>();
-        for(int[]p : pair){
-            double currTime = (double)(target - p[0]) / p[1];
-            if(stack.isEmpty()){
-                stack.push(currTime);
-                continue;
-            }
-            if(currTime>stack.peek()){
-                stack.push(currTime);
+        int count = 0;
+        float maxi = 0;
+        for (int i = time.length - 1; i >= 0; i--) {
+            if (time[i] > maxi) {
+                count++;
+                maxi = time[i];
             }
         }
-        return stack.size();
+        return count;
     }
 }
