4564055679878a050c334570d194628942791121
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-06-17 10:39:10 -0700

    Time: 3 ms (100%), Space: 43.8 MB (41.46%) - LeetHub

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.java
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.java
@@ -1,40 +1,55 @@
 class Solution {
+    // Modulo for all calculations
+    int mod = 1_000_000_007;
 
-    static final int MOD = (int) 1e9 + 7;
-    static final int MX = 100000;
-    static long[] fact = new long[MX];
-    static long[] invFact = new long[MX];
+    // Arrays to store factorials and modular inverses
+    static int[] f = new int[100001];      // Factorials
 
-    static long qpow(long x, int n) {
+    // Main function to count good arrays
+    public int countGoodArrays(int n, int m, int k) {
+        // Base case: factorial[0] = 1
+        if (f[0] == 0)
+            f[0] = 1;
+
+        // Formula:
+        // m               --> choose value for the first element
+        // pow(m-1, n-1-k) --> number of ways to keep same value (for n-1-k positions)
+        // C(n-1, n-1-k)   --> choose positions to remain the same among (n-1)
+        long res = m * pow(m - 1, n - 1 - k) % mod * C(n - 1, n - 1 - k) % mod;
+        
+        return (int) res;
+    }
+
+    // Fast exponentiation: computes (a^b) % mod
+    public long pow(int a, int b) {
         long res = 1;
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                res = (res * x) % MOD;
-            }
-            x = (x * x) % MOD;
-            n >>= 1;
+        long base = a;
+        while (b > 0) {
+            if ((b & 1) == 1)
+                res = res * base % mod;
+            base = base * base % mod;
+            b /= 2;
         }
         return res;
     }
 
-    static {
-        fact[0] = 1;
-        for (int i = 1; i < MX; i++) {
-            fact[i] = (fact[i - 1] * i) % MOD;
-        }
-        invFact[MX - 1] = qpow(fact[MX - 1], MOD - 2);
-        for (int i = MX - 1; i > 0; i--) {
-            invFact[i - 1] = (invFact[i] * i) % MOD;
-        }
+    // Computes nCr % mod using factorial and modular inverse
+    public long C(int a, int b) {
+        return (long) getF(a) * rev(getF(a - b)) % mod * rev(getF(b)) % mod;
     }
 
-    long comb(int n, int m) {
-        return (((fact[n] * invFact[m]) % MOD) * invFact[n - m]) % MOD;
+    // Computes factorial with memoization: f[a] = a!
+    public long getF(int a) {
+        if (f[a] != 0)
+            return f[a];
+        return f[a] = (int) (getF(a - 1) * a % mod);
     }
 
-    public int countGoodArrays(int n, int m, int k) {
-        return (int) ((((comb(n - 1, k) * m) % MOD) * qpow(m - 1, n - k - 1)) %
-            MOD);
+    // Modular inverse using Fermat's Little Theorem: a^(-1) ≡ a^(mod - 2)
+    public long rev(long a) {
+        if (a == 1)
+            return a;
+        return mod - mod / a * rev(mod % a) % mod;
     }
 }
 // n buckets, we can use m choices each, k adjacent
