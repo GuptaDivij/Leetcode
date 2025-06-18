b1dcdebdb06a3a960fa4d4fdf715c5a006144f35
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-06-17 21:39:19 -0700

    Time: 12 ms (98.75%), Space: 49.9 MB (73%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.java
+++ 0816-design-hashset/0816-design-hashset.java
@@ -1,20 +1,22 @@
 class MyHashSet {
     private boolean[][] hashset;
     public MyHashSet() {
-        this.hashset = new boolean[1000][1001];
+        this.hashset = new boolean[1000][];
     }
     
     public void add(int key) {
+        if(hashset[key%1000]==null && key%1000!=0) hashset[key%1000] = new boolean[1000];
+        if(hashset[key%1000]==null && key%1000==0) hashset[key%1000] = new boolean[1001];
         this.hashset[key%1000][key/1000] = true;
     }
     
     public void remove(int key) {
+        if(hashset[key%1000]==null) return;
         this.hashset[key%1000][key/1000] = false;
     }
     
     public boolean contains(int key) {
-
-        return this.hashset[key%1000][key/1000];
+        return hashset[key%1000]==null?false:this.hashset[key%1000][key/1000];
     }
 }
 
