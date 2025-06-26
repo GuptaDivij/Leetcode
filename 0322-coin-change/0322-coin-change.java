f0064885a2f36669c9e216c0224f681b91dc99ea
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-06-25 22:54:47 -0700

    Time: 14 ms (88.13%), Space: 44.7 MB (48%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,3 +1,12 @@
+// Time Complexity : O(amount * n) where n is coins length
+// Space Complexity : O(amount) for the array created
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach - I used an array to maintain a count of minimum coins needed to make up value of particular index.
+// At each point - I have 2 choices, incldue a coin -  I find it's previous amounts, if with any coin I see a previous amount that was made and using that + this coin has minimum steps, I update the index. Or not include - let the value be what it was.  
+// I return -1 if we cannot make amount, else the index amount from array as it represents the minimum coins.
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if(amount == 0) return 0;
