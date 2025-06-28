3502945a3c0072101175dba5fcce80f4025d8753
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-06-28 16:34:34 -0700

    Time: 7 ms (56.96%), Space: 44.9 MB (93.61%) - LeetHub

---
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int res = Integer.MAX_VALUE;
+        if(n==1){
+            for(int i = 0; i<n; i++) res = Math.min(res, matrix[0][i]);
+        }
+        for(int i = n-2; i>=0; i--){
+            for(int j = 0; j<n; j++){
+                int down = matrix[i+1][j];
+                int leftDiag = j-1>=0?matrix[i+1][j-1]:Integer.MAX_VALUE;
+                int rightDiag = j+1<n?matrix[i+1][j+1]:Integer.MAX_VALUE;
+                matrix[i][j] = matrix[i][j] + Math.min(down, Math.min(leftDiag, rightDiag));
+                if(i==0) res = Math.min(res, matrix[i][j]);
+            }
+        }
+        return res;
+    }
+}
+
+// start from second last row LOL
