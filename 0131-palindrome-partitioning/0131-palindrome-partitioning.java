015176ae8013fa66def063d56b5864b4afe9027f
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-05 13:32:59 -0700

    Time: 7 ms (97.82%), Space: 56.9 MB (69.1%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -22,15 +22,8 @@ class Solution {
 	}
 
 	public static boolean palindrome(String s) {
-		int i = 0;
-		int j = s.length() - 1;
-		while (i < j) {
-			if (s.charAt(i) != s.charAt(j)) {
-				return false;
-			}
-			i++;
-			j--;
-		}
+		int i = 0, j = s.length() - 1;
+		while (i < j) if (s.charAt(i++) != s.charAt(j--)) return false;
 		return true;
-}
+    }
 }
