0630bf1bcddce0f6aa547fee07bbc5d4b32fd057
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-07 14:49:26 -0700

    Time: 2 ms (61.39%), Space: 44.4 MB (52.22%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,23 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 class Solution {
-    private int i = 0;
-    private int p = 0;
-
+    private int p;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return build(preorder, inorder, Integer.MIN_VALUE);
+        HashMap<Integer, Integer> inOrder = new HashMap<>();
+        p=0;
+        for(int i = 0; i<inorder.length; i++){
+            inOrder.put(inorder[i],i);
+        }
+        return helper(preorder, 0, inorder.length-1, inOrder);
     }
+    private TreeNode helper(int[] preorder, int inStart, int inEnd, HashMap<Integer, Integer> inorderMap){
+        if (inStart > inEnd) return null;
 
-    private TreeNode build(int[] preorder, int[] inorder, int stop) {
-        if (p >= preorder.length) {
-            return null;
-        }
-        if (inorder[i] == stop) {
-            ++i;
-            return null;
-        }
+        TreeNode root = new TreeNode(preorder[p++]); // create a new node. 
+        int inorderIndex = inorderMap.get(root.val); // index at which this point is in inorder
 
-        TreeNode node = new TreeNode(preorder[p++]);
-        node.left = build(preorder, inorder, node.val);
-        node.right = build(preorder, inorder, stop);
-        return node;
+        root.left = helper(preorder, inStart, inorderIndex-1, inorderMap); // all elements to the left
+        root.right = helper(preorder, inorderIndex+1, inEnd, inorderMap); // all elements to the right
+
+        return root;
     }
 }
