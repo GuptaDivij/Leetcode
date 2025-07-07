360c7568b4d3cd76895e7eb21c2794ca3b50f36f
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-07 15:34:12 -0700

    Time: 0 ms (100%), Space: 42.2 MB (17.8%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if(digits[digits.length-1]!=9) {
+            digits[digits.length-1] += 1;
+            return digits;
+        }
+        int carry = 0;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
+            if (i == digits.length - 1) sum++;  // plus one on the last digit
+            carry = sum / 10;
+            digits[i] = sum % 10;
+        }
+        if (carry == 0) return digits;
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
