7411aa9a490566e7970516072a37bc246976a24c
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-09 23:07:51 -0700

    Time: 4 ms (78.21%), Space: 61.7 MB (44.87%) - LeetHub

--- 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.java
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.java
@@ -1,48 +1,33 @@
 class Solution {
-    public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
-        int n = startTime.length;
+    public int maxFreeTime(int eventTime, int[] start, int[] end) {
+        int n = start.length;
+        boolean[] movable = new boolean[n];
 
-        int[] gaps = new int[n + 1];
-        gaps[0] = startTime[0];
-        for (int i = 1; i < n; i++) {
-            gaps[i] = startTime[i] - endTime[i - 1];
-        }
-        gaps[n] = eventTime - endTime[n - 1];
+        int maxGapLeft = 0, maxGapRight = 0;
 
-        int[] prefMax = new int[n + 1];
-        int[] suffMax = new int[n + 2];
-        prefMax[0] = gaps[0];
-        for (int i = 1; i <= n; i++) prefMax[i] = Math.max(prefMax[i - 1], gaps[i]);
-        suffMax[n] = gaps[n];
-        for (int i = n - 1; i >= 0; i--) suffMax[i] = Math.max(suffMax[i + 1], gaps[i]);
-
-        int ans = prefMax[n];
-
+        // left-to-right scan
         for (int i = 0; i < n; i++) {
-            int dur = endTime[i] - startTime[i];
-            int largestElsewhere = Math.max(i > 0 ? prefMax[i - 1] : 0,
-                                            (i + 2 <= n) ? suffMax[i + 2] : 0);
-            if (largestElsewhere >= dur) {
-                int merged = gaps[i] + dur + gaps[i + 1];
-                ans = Math.max(ans, merged);
-            }
+            int dur = end[i] - start[i];
+            if (dur <= maxGapLeft) movable[i] = true;
+            maxGapLeft = Math.max(maxGapLeft, start[i] - (i == 0 ? 0 : end[i - 1]));
         }
 
+        // right-to-left scan
+        for (int i = n - 1; i >= 0; i--) {
+            int dur = end[i] - start[i];
+            if (dur <= maxGapRight) movable[i] = true;
+            maxGapRight = Math.max(maxGapRight,
+                                   (i == n - 1 ? eventTime : start[i + 1]) - end[i]);
+        }
+
+        int best = 0;
         for (int i = 0; i < n; i++) {
-            int dur = endTime[i] - startTime[i];
-            int prevEnd = (i == 0) ? 0 : endTime[i - 1];
-            int nextStart = (i == n - 1) ? eventTime : startTime[i + 1];
-
-            int newStart = nextStart - dur;
-            if (newStart >= prevEnd) ans = Math.max(ans, newStart - prevEnd);
-
-            int newEnd = prevEnd + dur;
-            if (newEnd <= nextStart) ans = Math.max(ans, nextStart - newEnd);
-
-            if (dur <= startTime[0]) ans = Math.max(ans, startTime[0] - dur);
-            if (dur <= eventTime - endTime[n - 1])
-                ans = Math.max(ans, eventTime - endTime[n - 1] - dur);
+            int left  = (i == 0)     ? 0          : end[i - 1];
+            int right = (i == n - 1) ? eventTime  : start[i + 1];
+            int free  = movable[i] ? right - left
+                                   : right - left - (end[i] - start[i]);
+            best = Math.max(best, free);
         }
-        return ans;
+        return best;
     }
 }
