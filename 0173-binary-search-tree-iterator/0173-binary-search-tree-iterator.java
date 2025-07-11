446a0bc9f291a2df09aeb691ef53987e85f40c4b
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-11 10:42:04 -0700

    Time: 18 ms (14.98%), Space: 48.4 MB (39.07%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -1,53 +1,46 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode(int x) { val = x; }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class BSTIterator {
-    Stack<TreeNode> stack;
-
+    Queue<Integer> queue;
     public BSTIterator(TreeNode root) {
-        // Stack for the recursion simulation
-        this.stack = new Stack<TreeNode>();
-
-        // Remember that the algorithm starts with a call to the helper function
-        // with the root node as the input
-        this._leftmostInorder(root);
-    }
-
-    private void _leftmostInorder(TreeNode root) {
-        // For a given node, add all the elements in the leftmost branch of the tree
-        // under it to the stack.
-        while (root != null) {
-            this.stack.push(root);
-            root = root.left;
+        queue = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while(!stack.isEmpty() || root!=null){
+            while(root!=null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            queue.add(root.val);
+            root = root.right;
         }
     }
-
-    /**
-     * @return the next smallest number
-     */
+    
     public int next() {
-        // Node at the top of the stack is the next smallest element
-        TreeNode topmostNode = this.stack.pop();
-
-        // Need to maintain the invariant. If the node has a right child, call the
-        // helper function for the right child
-        if (topmostNode.right != null) {
-            this._leftmostInorder(topmostNode.right);
-        }
-
-        return topmostNode.val;
+        return queue.poll();
     }
-
-    /**
-     * @return whether we have a next smallest number
-     */
+    
     public boolean hasNext() {
-        return this.stack.size() > 0;
+        return !queue.isEmpty();
     }
 }
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
