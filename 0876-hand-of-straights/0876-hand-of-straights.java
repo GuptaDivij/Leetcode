b6d2834f6865fdc8c4d3e3b7e77588c0de7a4f7d
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-13 00:56:28 -0700

    Time: 22 ms (93.59%), Space: 45.9 MB (27.14%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -1,17 +1,44 @@
 class Solution {
+
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if(hand.length%groupSize!=0) return false;
-        Arrays.sort(hand);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int n : hand) map.put(n, map.getOrDefault(n,0) + 1);
-        for(int i = 0; i<hand.length; i++){
-            if(map.get(hand[i])==0) continue;
-            for(int j = 0; j<groupSize; j++){
-                int num = hand[i] + j;
-                if(!map.containsKey(num) || map.get(num)==0) return false;
-                map.put(num, map.get(num) - 1);
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        // HashMap to store the count of each card value
+        HashMap<Integer, Integer> cardCount = new HashMap<>();
+        for (int card : hand) {
+            int count = cardCount.getOrDefault(card, 0);
+            cardCount.put(card, count + 1);
+        }
+
+        for (int card : hand) {
+            int startCard = card;
+            // Find the start of the potential straight sequence
+            while (cardCount.getOrDefault(startCard - 1, 0) > 0) {
+                startCard--;
+            }
+
+            // Process the sequence starting from startCard
+            while (startCard <= card) {
+                while (cardCount.getOrDefault(startCard, 0) > 0) {
+                    // Check if we can form a consecutive sequence
+                    // of groupSize cards
+                    for (
+                        int nextCard = startCard;
+                        nextCard < startCard + groupSize;
+                        nextCard++
+                    ) {
+                        if (cardCount.getOrDefault(nextCard, 0) == 0) {
+                            return false;
+                        }
+                        cardCount.put(nextCard, cardCount.get(nextCard) - 1);
+                    }
+                }
+                startCard++;
             }
         }
+
         return true;
     }
 }
