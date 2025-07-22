2d074ba16067caacec6f7edd928018f09c1b11de
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-22 16:54:22 -0700

    Time: 50 ms (71.52%), Space: 56.9 MB (91.88%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,21 +1,20 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
-        int left = 0, right = 0;
-        int maxSum = 0, windowSum = 0;
+        int n = nums.length;
+        HashMap<Integer, Integer> lastIndexMap = new HashMap<>();
+        int[] prefixSum = new int[n + 1];
 
-        while (right < nums.length) {
-            if (!seen.contains(nums[right])) {
-                seen.add(nums[right]);
-                windowSum += nums[right];
-                maxSum = Math.max(maxSum, windowSum);
-                right++;
-            } else {
-                seen.remove(nums[left]);
-                windowSum -= nums[left];
-                left++;
+        int result = 0, start = 0;
+        for (int end = 0; end < n; end++) {
+            int currentElement = nums[end];
+            prefixSum[end + 1] = prefixSum[end] + currentElement;
+            if (lastIndexMap.containsKey(currentElement)) {
+                start = Math.max(start, lastIndexMap.get(currentElement) + 1);
             }
+            // update result with maximum sum found so far
+            result = Math.max(result, prefixSum[end + 1] - prefixSum[start]);
+            lastIndexMap.put(currentElement, end);
         }
-        return maxSum;
+        return result;
     }
 }
