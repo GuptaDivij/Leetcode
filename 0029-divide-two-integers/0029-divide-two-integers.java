28304b51537036b0fc325bf7327a72ecc1976546
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-25 11:05:52 -0700

    Time: 1 ms (71.38%), Space: 41 MB (59.98%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        // Edge case: overflow
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        // Use long to avoid overflow
+        long lDividend = dividend;
+        long lDivisor = divisor;
+
+        int negative = 0;
+
+        if (lDividend < 0) {
+            negative++;
+            lDividend = -lDividend;
+        }
+
+        if (lDivisor < 0) {
+            negative++;
+            lDivisor = -lDivisor;
+        }
+
+        int res = 0;
+
+        while (lDividend >= lDivisor) {
+            long tempDivisor = lDivisor;
+            int multiple = 1;
+
+            // Doubling until it exceeds
+            while (lDividend >= tempDivisor + tempDivisor) {
+                tempDivisor = tempDivisor + tempDivisor;
+                multiple = multiple + multiple;
+            }
+            lDividend = lDividend - tempDivisor;
+            res = res + multiple;
+        }
+        return (negative % 2 == 0) ? res : -res;
+    }
+}
