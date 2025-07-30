c37a1f471ca35433299b7f036ae74397f9835d15
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-29 21:52:03 -0700

    Time: 9 ms (93.24%), Space: 55.6 MB (52.82%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,58 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Trie trie = new Trie();
+        for(String word : dictionary) trie.insert(word);
+        String[] words = sentence.split(" ");
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i<words.length; i++){
+            String word = words[i];
+            Trie.TrieNode curr = trie.root;
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j<word.length(); j++){
+                int idx = word.charAt(j)-'a';
+                if(curr.children[idx]==null || curr.inEnd) break;
+                sb.append(word.charAt(j));
+                curr = curr.children[idx];
+            }
+            if(curr.inEnd) res.append(sb.toString());
+            else res.append(word);
+            if(i!=words.length-1) res.append(" ");
+
+        }
+        return res.toString();
+    }
+}
+
+class Trie {
+    class TrieNode{
+        boolean inEnd;
+        TrieNode[] children;
+        public TrieNode(){
+            inEnd = false;
+            children = new TrieNode[26];
+        }
+    }
+    TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    public void insert(String word) {
+        TrieNode curr = root;
+        for(int i = 0;i<word.length(); i++){
+            int child = word.charAt(i)-'a';
+            if(curr.children[child]==null){
+                curr.children[child]= new TrieNode();
+            }
+            curr = curr.children[child];
+        }
+        curr.inEnd = true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
