b1829617bf3873f0bdc7481c48ebc0ba19c00759
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-07-31 11:47:04 -0700

    Time: 62 ms (40.03%), Space: 61.8 MB (22.58%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(nlogk) for the heap
+// Space Complexity : O(k)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach : I used a min heap to store k elements, as soon as the size gets more than k, i pop the mininum element out. At end, the k largest elements are in the heap and the popped one is the smallest among them, i.e., the kth largest element.
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> heap = new PriorityQueue<>(); 
