b054d5d2a19b57c8b2079db3c5c2a08d5b9eca06
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-01 14:34:16 -0700

    Time: 4 ms (92.88%), Space: 46.4 MB (13.96%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,39 +1,25 @@
-// Time Complexity : O(log(n) + k)
-// Space Complexity : O(1) 
-// Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : No
-
-// Approach : I used binary search to find the right index of closest element, then I add k nearest elements from this and its left to x. I add in a LinkedList to avoid sorting later on.
-
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int right = binarySearch(arr, x);
-        int left = right - 1;
-        LinkedList<Integer> res = new LinkedList<>();
+        // Initialize binary search bounds
+        int left = 0;
+        int right = arr.length - k;
         
-        while (k > 0) {
-            int leftDistance = left >= 0 ? Math.abs(arr[left] - x) : Integer.MAX_VALUE;
-            int rightDistance = right < arr.length ? Math.abs(arr[right] - x) : Integer.MAX_VALUE;
-            if (leftDistance <= rightDistance) {
-                res.addFirst(arr[left]);
-                left--;
+        // Binary search against the criteria described
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
             } else {
-                res.addLast(arr[right]);
-                right++;
+                right = mid;
             }
-            k--;
         }
-        return res;
-    }
-
-    public int binarySearch(int[] arr, int x) {
-        int low = 0, high = arr.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == x) return mid;
-            if (arr[mid] > x) high = mid - 1;
-            else low = mid + 1;
+        
+        // Create output in correct format
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
         }
-        return low;
+        
+        return result;
     }
 }
