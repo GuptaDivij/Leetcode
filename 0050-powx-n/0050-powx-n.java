3a5425873c4721df1e2040b43cd2390376c7da62
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-01 13:19:28 -0700

    Time: 0 ms (100%), Space: 41.7 MB (94.25%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -3,7 +3,6 @@ class Solution {
         if(x==1) return 1;
         if(x==-1 && n%2==0) return 1;
         if(n==Integer.MIN_VALUE) return 0;
-        if(n==1) return x;
         if(n<0){
             x=1/x;
             n*=-1; 
