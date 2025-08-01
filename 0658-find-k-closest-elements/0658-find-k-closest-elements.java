b491c53dc1713fb5c893cfb847b1d1076c69a112
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-01 14:33:24 -0700

    Time: 8 ms (45.58%), Space: 45.8 MB (75.66%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(log(n) + k)
+// Space Complexity : O(1) 
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach : I used binary search to find the right index of closest element, then I add k nearest elements from this and its left to x. I add in a LinkedList to avoid sorting later on.
+
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int right = binarySearch(arr, x);
