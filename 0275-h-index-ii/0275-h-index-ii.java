b42b44fa8c3b735d523041f6a101baa77a549a11
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-04 19:21:13 -0700

    Time: 0 ms (100%), Space: 47.6 MB (96.59%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(log(n))
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes 
+// Any problem you faced while coding this : No
+
+// Approach : we just need to return the first index for which citations[i] is greater than or equal to n-i, because there will n-i papers remaining, and all of the number of citations ahead are gonna be greater as it is ascending. I used binary search to do it in logarithmic time as I can set a mid and keep moving low and high bounds till I find the right index - if nums[mid]>=n-mid, it means this satisfies our condition, n-mid papers are left and all of them will be greater, however, we want the first one to satisfy this as it will maximize n-mid, so we move high to mid-1. If nums[mid]<n-mid it means more papers are left but the citations we recieved are less, so we move low ahead to mid + 1.
+
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
