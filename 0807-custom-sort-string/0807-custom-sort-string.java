eeeaddaaa4780a69f3af8593376e64e3dc9d2f20
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-06 11:42:53 -0700

    Time: 2 ms (66.89%), Space: 41.8 MB (33.14%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.java
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -1,10 +1,16 @@
+// Time Complexity : O(s.length) because order will be always less than 26
+// Space Complexity : O(s.length) for map and sb
+// Did this code successfully run on Leetcode : Yes 
+// Any problem you faced while coding this : No
+
+// Approach : Add every char of s to the map with its frequency, and according to the order add it. If at end some characters are left, I can add in any order.
+
 class Solution {
     public String customSortString(String order, String s) {
         HashMap<Character, Integer> countMap = new HashMap<>();
         for (char c : s.toCharArray()) {
             countMap.put(c, countMap.getOrDefault(c, 0) + 1);
         }
-
         StringBuilder sb = new StringBuilder();
         for (char c : order.toCharArray()) {
             if (countMap.containsKey(c)) {
