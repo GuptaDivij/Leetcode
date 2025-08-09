c27d0d1b9ebb3987c6455a323a5f056f379f31f8
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-08 21:36:44 -0700

    Time: 14 ms (84.58%), Space: 45.1 MB (81.88%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,32 +1,39 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach : I kept track of last sign (i start with calculated = 0 and do operations in format 0+....), calculated value and tail (last value after last Op) -> if its a digit, I update the curr, else I do the operation -> if it was meant to be added (last sign determines) I add it, etc etc, now when I need to multiply or divide I updated calculated to be calculated - tail so calculated doesn't hold any last value and this value is multiplied or divided by curr and added to the calculated answer.
+
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int curr = 0;
-        char lastSign = '+';
+        if(s.length()==1) return s.charAt(0)-'0';
+        int calculated = 0, curr = 0, tail = 0;
+        char lastOp = '+';
         for(int i = 0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(Character.isDigit(c)){
-                curr = curr*10+(c-'0');
-            }
+            if(Character.isDigit(c)) curr = curr*10 + (c-'0');
             if((!Character.isDigit(c) && c!=' ') || i==s.length()-1){
-                if(lastSign=='+'){
-                    stack.push(curr);
+                if(lastOp=='+'){
+                    calculated += curr;
+                    tail = curr; 
                 }
-                if(lastSign=='-'){
-                    stack.push(-curr);
+                if(lastOp=='-'){
+                    calculated -= curr;
+                    tail = -curr;
                 }
-                if(lastSign=='*'){
-                    stack.push(stack.pop()*curr);
+                if(lastOp=='*'){
+                    calculated = calculated - tail + tail*curr;
+                    tail = tail*curr;
                 }
-                if(lastSign=='/'){
-                    stack.push(stack.pop()/curr);
+                if(lastOp=='/'){
+                    calculated = calculated - tail + tail/curr;
+                    tail = tail/curr;
                 }
-                lastSign = c;
                 curr=0;
+                lastOp=c;
             }
         }
-        int res = 0;
-        while(!stack.isEmpty()) res+= stack.pop();
-        return res;
+        return calculated;
     }
 }
