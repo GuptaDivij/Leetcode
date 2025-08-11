dd9fc894fd320d2683471d4f8364f634a1578460
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-11 10:06:18 -0700

    Time: 13 ms (73.16%), Space: 45 MB (60.75%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,33 +1,34 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
-        int[] tFreq = new int[128];
-        int[] sFreq = new int[128];
-        for (int i = 0; i < t.length(); i++) {
-            tFreq[t.charAt(i)]++;
+        if(t.length()>s.length()) return "";
+        int [] sWindow = new int[52];
+        int [] tWindow = new int[52];
+
+        for(int i = 0; i<t.length();i++){
+            char c = t.charAt(i);
+            tWindow[Character.isLowerCase(c)?c-'a':c-'A'+26]++;
         }
+
         int start = 0;
-        int minLen = Integer.MAX_VALUE;
-        int minStart = 0;
-        for (int end = 0; end < s.length(); end++) {
+        int [] best = new int[2];
+        best[0]=-1;
+        for(int end = 0; end<s.length(); end++){
             char c = s.charAt(end);
-            sFreq[c]++;
-            while (contains(sFreq, tFreq)) {
-                if (end - start + 1 < minLen) {
-                    minLen = end - start + 1;
-                    minStart = start;
+            sWindow[Character.isLowerCase(c)?c-'a':c-'A'+26]++;
+            while(contains(sWindow, tWindow)){
+                if(best[0]==-1 || (best[1]-best[0]) > (end-start)){
+                    best[0]=start;
+                    best[1]=end;
                 }
-                sFreq[s.charAt(start)]--;
-                start++;
+                c = s.charAt(start++);
+                sWindow[Character.isLowerCase(c)?c-'a':c-'A'+26]--;
             }
         }
-        if(minLen == Integer.MAX_VALUE) return "";
-        return s.substring(minStart, minStart + minLen);
+        return best[0]==-1?"":s.substring(best[0], best[1]+1);
     }
-
-    private boolean contains(int[] sFreq, int[] tFreq) {
-        for (int i = 0; i < 128; i++) {
-            if (sFreq[i] < tFreq[i]) return false;
+    public boolean contains(int[]s,int[]t){
+        for(int i = 0; i<52; i++){
+            if(t[i]>s[i]) return false;
         }
         return true;
     }
