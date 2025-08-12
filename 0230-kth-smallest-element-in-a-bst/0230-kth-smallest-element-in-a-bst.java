bd9e966ec439fc1abcd9e9f1601a621d31fe0ef1
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-12 10:24:56 -0700

    Time: 0 ms (100%), Space: 44.5 MB (61.02%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(n)
+// Space Complexity : O(h) - height of the tree
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach : I did a DFS in order - it will be in sorted order since it is a BST, i.e., all left values are smaller than root and right are greater than it. Everytime I pop - it is the current smallest element, so I reduce k. Once k==0, it means I am on the right node and I can return it. Else I keep traversing inorder.
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
