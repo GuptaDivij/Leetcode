51ff6b5a6fee3a9119e17c86f0fdbb7e464f5f0e
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-17 11:25:46 -0700

    Time: 1 ms (79.02%), Space: 41.6 MB (65.28%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -1,24 +1,18 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        // The last day on which we need to travel.
         int lastDay = days[days.length - 1];
-        int dp[] = new int[lastDay + 1];
-        Arrays.fill(dp, 0);
-
+        int minCost[] = new int[lastDay + 1];
         int i = 0;
         for (int day = 1; day <= lastDay; day++) {
-            // If we don't need to travel on this day, the cost won't change.
-            if (day < days[i]) {
-                dp[day] = dp[day - 1];
-            } else {
-                // Buy a pass on this day, and move on to the next travel day.
+            if (day < days[i]) minCost[day] = minCost[day - 1];
+            else {
+                int oneDay = costs[0] + minCost[day-1];
+                int sevenDay = costs[1] + minCost[Math.max(0,day-7)];
+                int thirtyDay = costs[2] + minCost[Math.max(0,day-30)];
+                minCost[day] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
                 i++;
-                // Store the cost with the minimum of the three options.
-                dp[day] = Math.min(dp[day - 1] + costs[0],Math.min(dp[Math.max(0, day - 7)] + costs[1],
-                                dp[Math.max(0, day - 30)] + costs[2]));
             }
         }
-
-        return dp[lastDay];
+        return minCost[lastDay];
     }
 }
