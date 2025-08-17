b7b6531c0e9f690ccca6b9bdaf49e67f9a5dbb7c
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-08-17 11:14:46 -0700

    Time: 1 ms (79.02%), Space: 41.5 MB (76.26%) - LeetHub

---
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int [] minCost = new int[366]; 
+
+        for (int i = 0; i < days.length; i++) {
+            int day = days[i];
+            int oneDay = costs[0] + minCost[findClosestDay(days, 1, i)];
+            int sevenDay = costs[1] + minCost[findClosestDay(days, 7, i)];
+            int thirtyDay = costs[2] + minCost[findClosestDay(days, 30, i)];
+            minCost[day] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
+        }
+        return minCost[days[days.length - 1]];
+    }
+
+    public int findClosestDay(int[] days, int period, int idx) {
+        for (int i = idx - 1; i >= 0; i--) {
+            if (period <= days[idx] - days[i]) {
+                return days[i];
+            }
+        }
+        return 0; 
+    }
+}
