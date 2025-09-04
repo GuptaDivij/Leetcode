bd9cb4bfc906e04c205f05d05b4f4c99a729b948
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-09-04 11:12:03 -0700

    Time: 0 ms (100%), Space: 40.3 MB (77.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(n*m) m is number of rows and n is number of columns
+// Space Complexity : O(n) for rows
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach : I am using a DP approach because of repeated subproblem and exhaustive search problem - for the first row there is only one way to reach each index - go right, so all are filled with 1. For first column, only one way - to go down. Now for each index, the total number of ways to reach there depends on 2 points - the point above (we can come down) and the point to its left (we can go right), so we just add the total ways we can come to these points. Each index represents the number of ways we can come to that point - we can use 2D array for comprehensive answer but we only care about the last point, so we can optimize by only keeping the last row and making new one based off of it. 
+
 class Solution {
     public int uniquePaths(int m, int n) {
         int[] prevRow = new int[n];
