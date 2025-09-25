4433ca65fbcf1fa7b965f7c4b62af6e7b63cf2b7
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-09-25 10:34:24 -0700

    Time: 3 ms (20.41%), Space: 44.5 MB (46.97%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.java
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -1,18 +1,23 @@
+// Time complexity = O(n + K)
+// Space complexity = O(n)
+
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n = rooms.size();
         HashSet<Integer> visited = new HashSet<>();
         Queue<Integer> queue = new LinkedList<>();
         queue.add(0);
+        visited.add(0);
         while(!queue.isEmpty()){
             // visit the room
             int room = queue.poll();
-            visited.add(room);
             List<Integer> keys = rooms.get(room);
             for(int key : keys){
                 if(visited.contains(key)) continue;
+                visited.add(key);
                 queue.add(key);
             }
+            if(visited.size()==n) return true;
         }
         return visited.size()==n;
     }
