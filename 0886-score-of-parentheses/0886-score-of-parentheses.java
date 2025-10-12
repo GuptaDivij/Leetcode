c909c1fe2efc72ff9192634316014e4ecd6e81db
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-10-12 12:50:41 -0700

    Time: 0 ms (100%), Space: 41.9 MB (7.97%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.java
+++ 0886-score-of-parentheses/0886-score-of-parentheses.java
@@ -10,6 +10,9 @@ class Solution {
                 // update multiplier
                 multiplier /= 2;  
                 // if we just closed a "()", add current multiplier
+                // ((( ))) in the most inside one we have multiplier = 4 we don't need other score.
+                // If () if after this finishes, we need to start afresh with multiplier = 1
+                // multiplier will never go below 1 as we know it is balanced
                 if (s.charAt(i - 1) == '(') {
                     score += multiplier;
                 }
