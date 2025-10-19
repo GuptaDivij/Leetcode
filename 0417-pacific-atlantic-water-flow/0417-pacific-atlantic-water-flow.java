26b2cdc5492866dc5b3b9c7738c37cac83540e91
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2025-10-19 10:23:10 -0700

    Time: 4 ms (98.12%), Space: 46.3 MB (25.68%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,69 +1,36 @@
 class Solution {
-    int [][] visited;
-    int [][] visitA;
-    int [][] visitP;
-    int [][] heights;
-    int [][] dirs;
+    private int m, n;
+    private int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int m = heights.length;
-        int n = heights[0].length;
-        this.heights = heights;
-        dirs = new int[][]{{-1,0}, {0,-1}, {0,1}, {1, 0}};
-        visited = new int[m][n]; 
-        visitA = new int[m][n]; // if i can traverse to ocean A
-        visitP = new int[m][n]; // if i can traverse to ocean B
-        for(int j = 0; j<n;j++){
-            visitP[0][j] = 1;
-        }
-        for(int i = 0; i<m;i++){
-            visitP[i][0] = 1;
-        }
-        for(int j = 0; j<n;j++){
-            visitA[m-1][j] = 1;
-        }
-        for(int i = 0; i<m;i++){
-            visitA[i][n-1] = 1;
-        }
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                visited = new int[m][n];
-                dfs(i, j);
-            }
-        }
-        List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(visitA[i][j]==1 && visitP[i][j]==1){
-                    List<Integer> currentPoint = new ArrayList<>();
-                    currentPoint.add(i);
-                    currentPoint.add(j);
-                    res.add(currentPoint);
+        m = heights.length;
+        n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        for (int j = 0; j < n; j++) dfs(0, j, heights, pacific);
+        for (int i = 0; i < m; i++) dfs(i, 0, heights, pacific);
+        for (int j = 0; j < n; j++) dfs(m - 1, j, heights, atlantic);
+        for (int i = 0; i < m; i++) dfs(i, n - 1, heights, atlantic);
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    result.add(Arrays.asList(i, j));
                 }
             }
         }
-        return res;
+        return result;
     }
 
-    public void dfs(int i, int j){
-        if(visited[i][j] != 0) return;
-        visited[i][j] = 1;
-        for(int[] dir : dirs){
-            int row = i+dir[0];
-            int col = j+dir[1];
-            if(row>=0 && row<heights.length && col>=0 && col<heights[0].length){
-                // if i can flow, I will call dfs
-                if(heights[row][col] <= heights[i][j]){
-                    dfs(row, col);
-                    if(visitA[row][col]==1){
-                        visitA[i][j] = 1;
-                    }
-                    if(visitP[row][col]==1){
-                        visitP[i][j] = 1;
-                    }
-                }
-            }
+    private void dfs(int i, int j, int[][] heights, boolean[][] visited) {
+        if (visited[i][j]) return;
+        visited[i][j] = true;
+        for (int[] d : directions) {
+            int x = i + d[0], y = j + d[1];
+            if (x < 0 || x >= m || y < 0 || y >= n) continue;
+            if (heights[x][y] < heights[i][j]) continue;
+            dfs(x, y, heights, visited);
         }
-
-        
     }
 }
