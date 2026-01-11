04362526fdb1109b32f688ea7f8d4b3a73c15104
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-01-10 21:30:36 -0800

    Time: 3 ms (97.15%), Space: 77.3 MB (52.73%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach : I use two pointers from left and right, I use the formula of area -> height x width, width is right - left and height is the lowest of heigh[left] and height[rigth], as otherwise the water will overflow. I also use a greedy approach to maximize the overall water by moving the index with lower height ahead. 
+
 class Solution {
     public int maxArea(int[] height) {
         int max = 0;
