456021e2c7c23a00ef5bd16ffc1520741b1b343b
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-01-11 00:20:24 -0800

    Time: 112 ms (31.38%), Space: 48.6 MB (6.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,21 +1,22 @@
 class Solution {
     public String longestPalindrome(String s) {
+        int start = 0; 
+        int end = 0;
         int n = s.length();
-        if(n==1) return s;
-        int[]range = new int[2];
-        boolean [][]dp = new boolean[n][n];
+        boolean [][] dp = new boolean[n][n];
         for(int i = 0; i<n; i++) dp[i][i] = true;
         for(int i = 1; i<n; i++){
-            for(int j = 0; j<n-i; j++){
-                if (s.charAt(j) == s.charAt(j + i) && (i == 1 || dp[j + 1][j + i - 1])){
+            // other points
+            for (int j = 0; j<n-i; j++){
+                if(s.charAt(j)==s.charAt(j+i) && (i==1 || dp[j+1][j+i-1])){
                     dp[j][j+i] = true;
-                    if(i>(range[1]-range[0])) {
-                        range[0]=j;
-                        range[1]=j+i; 
+                    if(i>end-start){
+                        end = j+i;
+                        start = j;
                     }
                 }
             }
         }
-        return s.substring(range[0],range[1]+1);
+        return s.substring(start, end+1);
     }
 }
