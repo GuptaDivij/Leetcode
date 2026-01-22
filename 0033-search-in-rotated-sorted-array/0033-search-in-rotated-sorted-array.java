1b43ac047d5783f25b948833b0e9e028d6f39044
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-01-22 07:56:18 -0600

    Time: 0 ms (100%), Space: 44.2 MB (6.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,18 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) right = mid - 1;
-                else left = mid + 1;
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            // if nums[mid] > nums[low], it is ascending till mid and we can directly perform binary search in this if target is in middle of this
+            if(nums[low]<=nums[mid]){
+                if(nums[mid]>target && target>=nums[low]) high = mid-1;
+                else low = mid+1;
             }
-            else {
-                if (nums[mid] < target && target <= nums[right]) left = mid + 1;
-                else right = mid - 1;
+            // else, mid is lower than low, so it is ascending from mid to high, if it exist in that range, we update
+            else{
+                if(target>nums[mid] && target<=nums[high]) low = mid+1;
+                else high = mid-1;
             }
         }
-        return -1; 
+        return -1;
     }
 }
