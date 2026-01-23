3e6697381fa8c780868ebfb090ad5f1e21787442
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-01-23 13:43:45 -0600

    Time: 0 ms (100%), Space: 42.9 MB (26.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,12 @@
-// Time Complexity : O(n) where n is nums length
-// Space Complexity : O(1) 
-// Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : No
-
-// Approach - Using two variables to store values of maximum money we can get from either previous1 exact neighbor or previous 2 (i-2) house.
-// Now I have two choices, I can include this house or exclude it, including, I add the money from this house and alternate before, else the previous one. 
-
 class Solution {
     public int rob(int[] nums) {
         if(nums.length==1) return nums[0];
-        if(nums.length==2) return nums[0]>nums[1]?nums[0]:nums[1];
-        int prev2 = nums[0];
-        int prev1 = Math.max(nums[0], nums[1]);
-        for(int i = 2; i<nums.length;i++){
-            int include = prev2 + nums[i];
-            int exclude = prev1;
-            prev2 = prev1;
-            prev1 = Math.max(include,exclude);
+        int[]dp = new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0], nums[1]);
+        for(int i = 2; i<nums.length; i++){
+            dp[i] = Math.max(dp[i-1], nums[i]+dp[i-2]);
         }
-        return prev1;
+        return dp[nums.length-1];
     }
 }
