107f73b022025074048e785aad8b707bd8962fa1
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-01-25 15:00:07 -0600

    Time: 3 ms (54.93%), Space: 48.1 MB (31.96%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.java
+++ 0690-employee-importance/0690-employee-importance.java
@@ -1,16 +1,27 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
 class Solution {
     public int getImportance(List<Employee> employees, int id) {
-        Map<Integer, Employee> map = new HashMap<>();
-        for (Employee emp : employees) map.put(emp.id, emp);
-        int totalImportance = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(id);
-        while (!queue.isEmpty()) {
-            int currentId = queue.poll();
-            Employee emp = map.get(currentId);
-            totalImportance += emp.importance;
-            for (int subId : emp.subordinates) queue.offer(subId);
+        int importance = 0;
+        HashMap<Integer, Employee> map = new HashMap<>();
+        for(Employee employee: employees) map.put(employee.id, employee);
+        Queue<Integer> level = new LinkedList<>();
+        level.add(id);
+        while(!level.isEmpty()){
+            int curr = level.poll();
+            importance += map.get(curr).importance;
+            List<Integer> list = map.get(curr).subordinates;
+            for(int ids : list) level.add(ids);
         }
-        return totalImportance;
+        return importance;
     }
 }
+// HashMap -> id, emp
+// BFS - for this emp, get all employees, add subs in queue
