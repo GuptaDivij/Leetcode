73c8d25c87141674e4c2fda15b6cca9d86962e9f
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-01-27 15:00:04 -0600

    Time: 4 ms (24.12%), Space: 47 MB (46.36%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.java
+++ 0529-minesweeper/0529-minesweeper.java
@@ -1,49 +1,61 @@
 class Solution {
-    int[][] dirs;
+    int[][] dirs = {
+        {-1,0}, {1,0}, {0,-1}, {0,1},
+        {-1,-1}, {-1,1}, {1,-1}, {1,1}
+    };
+
     public char[][] updateBoard(char[][] board, int[] click) {
-        if(board[click[0]][click[1]]=='M'){
-            board[click[0]][click[1]]='X';
+        int r = click[0];
+        int c = click[1];
+
+        // Clicked on a mine
+        if (board[r][c] == 'M') {
+            board[r][c] = 'X';
             return board;
         }
+
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(click);
-        dirs = new int[][]{{-1,0}, {1,0}, {0,-1}, {0,1}, {-1,-1}, {-1,1}, {1,-1}, {1,1}};
-        while(!queue.isEmpty()){
+        queue.offer(new int[]{r, c});
+
+        while (!queue.isEmpty()) {
             int[] curr = queue.poll();
             int row = curr[0];
             int col = curr[1];
-            if (board[row][col] == 'M') { 
-                board[row][col] = 'X';
-            }
-            else{
-                int count = countMines(board, row, col);
-                if (count > 0) { 
-                    board[row][col] = (char)(count + '0');
-                }
-                else {
-                    board[row][col] = 'B';
-                    for(int[] dir : dirs){
-                        int nr = row + dir[0];
-                        int nc = col + dir[1];
-                        if(nr<board.length && nc<board[0].length && nr>=0 && nc>=0 && board[nr][nc]=='E'){
-                            queue.offer(new int[]{nr, nc});
-                            board[nr][nc] = 'B';
-                        }
+
+            if (board[row][col] != 'E') continue;
+
+            int count = countMines(board, row, col);
+
+            if (count > 0) {
+                board[row][col] = (char) (count + '0');
+            } else {
+                board[row][col] = 'B';
+                for (int[] dir : dirs) {
+                    int nr = row + dir[0];
+                    int nc = col + dir[1];
+                    if (nr >= 0 && nr < board.length &&
+                        nc >= 0 && nc < board[0].length &&
+                        board[nr][nc] == 'E') {
+                        queue.offer(new int[]{nr, nc});
                     }
                 }
             }
         }
+
         return board;
     }
-    public int countMines(char[][] board, int row, int col){
-        int ct = 0;
-        for(int[] dir : dirs){
+
+    private int countMines(char[][] board, int row, int col) {
+        int count = 0;
+        for (int[] dir : dirs) {
             int nr = row + dir[0];
             int nc = col + dir[1];
-            if(nr<board.length && nc<board[0].length && nr>=0 && nc>=0 && board[nr][nc]=='M'){
-                ct++;
+            if (nr >= 0 && nr < board.length &&
+                nc >= 0 && nc < board[0].length &&
+                board[nr][nc] == 'M') {
+                count++;
             }
         }
-        return ct;
+        return count;
     }
 }
