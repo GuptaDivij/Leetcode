6ee0e36b7569cee12c259671380940d0db510935
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-01-28 14:03:13 -0600

    Time: 12 ms (82.93%), Space: 51.8 MB (5.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,21 +1,35 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int [] ans = new int[k];
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-        for(int num : nums){
-            freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
+
+        // Buckets where index = frequency
+        List<List<Integer>> bucket = new ArrayList<>();
+        for (int i = 0; i <= nums.length; i++) {
+            bucket.add(new ArrayList<>());
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> freqMap.get(a) - freqMap.get(b));
-        for(int key : freqMap.keySet()){
-            pq.add(key);
-            if(pq.size()>k){
-                pq.poll();
+
+        // Fill buckets
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            bucket.get(freq).add(num);
+        }
+
+        // Collect result from highest frequency to lowest
+        List<Integer> result = new ArrayList<>();
+        for (int i = bucket.size() - 1; i >= 0 && result.size() < k; i--) {
+            for (int num : bucket.get(i)) {
+                result.add(num);
+                if (result.size() == k) break;
             }
         }
-        int i = 0;
-        while (!pq.isEmpty()) {
-            ans[i++] = pq.poll();
+
+        // Convert to array
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = result.get(i);
         }
-        return ans;
+        return res;
     }
 }
