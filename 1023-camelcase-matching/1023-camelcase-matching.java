2a4c5cb41089fc9f7ecea968df779323a0ee1f47
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-01-30 10:38:31 -0600

    Time: 0 ms (100%), Space: 43.2 MB (34.6%) - LeetHub

---
+++ 1023-camelcase-matching/1023-camelcase-matching.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        List<Boolean> res = new ArrayList<>();
+        for(String query: queries) res.add(func(query, pattern));
+        return res;
+    }
+    public boolean func(String query, String pattern){
+        if(pattern.length()>query.length()) return false;
+        int k = 0;
+        for(int i = 0; i<query.length(); i++){
+            if(k < pattern.length() && query.charAt(i)==pattern.charAt(k)) k++;
+            else if(query.charAt(i)-'a'<0) return false; // capital letters
+        }
+        return k==pattern.length();
+    }
+}
