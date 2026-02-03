10b2f3d01d0cb790d398297a9382eb5d54df4a51
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-02-03 14:45:57 -0600

    Time: 88 ms (17.89%), Space: 90.9 MB (5.2%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -3,28 +3,38 @@ class Solution {
     private HashSet<Integer> lengths;
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> res = new ArrayList<>();
+        // O(1)
         trie = new Trie();
+        // O(1)
         lengths = new HashSet<>();
+        // O(sum(len))
         for(String word : words) {
             trie.addWord(word);
             lengths.add(word.length());
         }
         // now trie has every word, I just need to check words if they are made up of other words or not
+        // O(n * len^2 * k)
         for(String word : words){
+            // O(len^2 * k)
             if(concatenatedWord(word)) res.add(word);
         }
         return res;
     }
 
+    // O(len^2 * k)
     private boolean concatenatedWord(String word){
+        // O(1)
         boolean[] contained = new boolean[word.length()+1];
         contained[0] = true;
-        for(int i = 0; i<word.length(); i++){
+        // O(len^2 * k)
+        for(int i = 0; i<word.length(); i++){ // O(len)
             if(!contained[i]) continue;
-            for(int len : lengths){
+            // O(k * len)
+            for(int len : lengths){ 
                 if(i + len > word.length()) continue;
-                String check = word.substring(i, i+len);
+                String check = word.substring(i, i+len); // O(len)
                 if(check.equals(word)) continue;
+                // O(len)
                 if(trie.containsWord(check)) contained[i+len] = true;
                 if(contained[word.length()]) return true;
             }
@@ -38,14 +48,17 @@ class Trie{
     class TrieNode{
         TrieNode[] children;
         boolean isEnd;
+        // O(1)
         public TrieNode(){
             children = new TrieNode[26];
             isEnd = false;
         }
     }
+    // O(1) 
     public Trie(){
         root = new TrieNode();
     }
+    // O(len)
     public void addWord(String word){
         TrieNode curr = root;
         for(int i = 0; i<word.length(); i++){
@@ -55,6 +68,7 @@ class Trie{
         }
         curr.isEnd = true;
     }
+    // O(len)
     public boolean containsWord(String word){
         TrieNode curr = root;
         for(int i = 0; i<word.length(); i++){
