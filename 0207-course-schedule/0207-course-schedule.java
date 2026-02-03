1e77431c3881da92e8aac3a4ba40f91cc0281cc6
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-02-03 11:33:58 -0600

    Time: 9 ms (23.13%), Space: 47.4 MB (31.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,34 +1,33 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<Integer>[] arr = new ArrayList[numCourses];
-        int[] courseDependencies = new int[numCourses];
-        for(int [] courseList: prerequisites){
-            int course = courseList[0];
-            int preReq = courseList[1];
-            if(arr[preReq] == null) arr[preReq] = new ArrayList<Integer>();
-            arr[preReq].add(course);
-            courseDependencies[course]++;
+        // bi allows ai
+        // bfs - first do courses with 0 reqs
+        // maintain an arr with required courses -> 
+        // at each step if you do a course, update all others that depend on it
+        // we need int reqs[n]; and Map(a,b) a->b b is list of all courses we can do after a
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int [] reqsLeft = new int [numCourses];
+        for(int [] pair : prerequisites){
+            int a = pair[0];
+            int b = pair[1];
+            map.computeIfAbsent(b, k -> new ArrayList<>()).add(a); // list of courses that b allows
+            reqsLeft[a]++; // a has to complete these many courses before it can start
         }
-        int finishedCourses = 0;
-        Queue<Integer> currentCourse = new LinkedList<>();
-        for(int i = 0; i<numCourses; i++) {
-            if(courseDependencies[i] == 0){
-                finishedCourses++;
-                currentCourse.add(i);
+        Queue<Integer> taking = new LinkedList<>();
+        for(int i = 0; i<numCourses; i++){
+            if(reqsLeft[i]==0) taking.offer(i); // if a course has no prereqs - do it
+        }
+        while(!taking.isEmpty()){
+            int finished = taking.poll(); // finished this course
+            numCourses--;
+            // list of courses to which this was a prereq
+            List<Integer> canDo = map.containsKey(finished)?map.get(finished):null;
+            if(canDo==null) continue;
+            for(int course : canDo){
+                reqsLeft[course]--;
+                if(reqsLeft[course] == 0) taking.offer(course);
             }
         }
-        while(!currentCourse.isEmpty()){
-            int course = currentCourse.poll();
-            if(arr[course]==null) continue;
-            ArrayList<Integer> nextCourses = arr[course];
-            for(int next: nextCourses){
-                courseDependencies[next]--;
-                if(courseDependencies[next] == 0){
-                    finishedCourses++;
-                    currentCourse.add(next);
-                }
-            }
-        }
-        return numCourses == finishedCourses;
+        return numCourses == 0;
     }
 }
