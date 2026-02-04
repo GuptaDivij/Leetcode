fdf26ffcf2d802754cf92004d561617573f4bb67
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-02-04 12:23:51 -0600

    Time: 0 ms (100%), Space: 43 MB (9.66%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -9,3 +9,6 @@ class Solution {
         return res;
     }
 }
+// basically arr[i] should be at i
+// if it is not, this needs to be sorted for the whole chunk until it's correct place which is i
+// so max end for current chunk is max of curr max and arr[i], if we reach end, we can increase the chunk size that everything before it is sorted, now we need new chunks
