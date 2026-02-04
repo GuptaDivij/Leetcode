6ac98634d2353794bde8bc02e59364a0e8155ff7
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-02-04 12:16:03 -0600

    Time: 0 ms (100%), Space: 46.7 MB (44.72%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,14 +1,13 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach : I use two pointers fast and slow. Fast moves twice the speed of slow. When they meet, slow has covered distance till start of cycle (i) + some distance in the cycle (j), whereas fast has covered twice of it, which is also equal to distance till start of cycle (i) + n cycles + some distance in the cycle (j). -> i + nx + j
+// Also, fast covered twice distance, so nx+i+j = 2i+2j. So, nx=i+j => nx-j=i. 
+// Fast is currently at nx+j inside the cycle, moving slow to 0 and moving it ahead till it meats fast will give us the start of the cycle. This is because, slow will move i steps, and after i steps, fast will be at nx+j+i = 
+// nx+j+nx-j=2nx. Basically 2x cycles without any additional steps -> this is the beginning of the cycle.
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         if(head == null || head.next == null) return null;
