91a4768e77472a5f60838635e2d33e98137a1ad9
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2026-02-11 21:10:57 -0600

    Time: 91 ms (5.31%), Space: 103.8 MB (5.02%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,19 +1,43 @@
 class Solution {
-    public int maxFrequency(int[] nums, int k) {
-        Arrays.sort(nums);
+    public int check(int i, int k, int[] nums, long[] prefix) {
+        int target = nums[i];
         int left = 0;
-        long curr = 0;
+        int right = i;
+        int best = i;
         
-        for (int right = 0; right < nums.length; right++) {
-            long target = nums[right];
-            curr += target;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long count = i - mid + 1;
+            long finalSum = count * target;
+            long originalSum = prefix[i] - prefix[mid] + nums[mid];
+            long operationsRequired = finalSum - originalSum;
             
-            if ((right - left + 1) * target - curr > k) {
-                curr -= nums[left];
-                left++;
+            if (operationsRequired > k) {
+                left = mid + 1;
+            } else {
+                best = mid;
+                right = mid - 1;
             }
         }
         
-        return nums.length - left;
+        return i - best + 1;
+    }
+    
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        long[] prefix = new long[nums.length];
+        prefix[0] = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = nums[i] + prefix[i - 1];
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = Math.max(ans, check(i, k, nums, prefix));
+        }
+        
+        return ans;
+        
     }
 }
